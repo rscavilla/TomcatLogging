4badc9fa38d85f780b7f9b0884f6f33b46eac5d1
Bob on Deacon <bob@fourfront.us>
2024-12-16 13:34:21 -0500

    Initial Commit

---
+++ TomcatLogging/src/main/java/us/fourfront/App/App.java
@@ -0,0 +1,32 @@
+package us.fourfront.App;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("/app")
+public class App extends ResourceConfig {
+
+	private static final Logger logger = LogManager.getLogger(App.class);
+	
+	public App() {
+		
+		ThreadContext.put("tcport", "App");
+		
+		try {
+			System.setProperty("hostName", InetAddress.getLocalHost().getHostName());
+		} catch (UnknownHostException e) {
+			logger.error("Failed to set hostname");
+		}
+		
+		logger.info("Starting us.fourfront.App class...");
+
+	}
+
+}
---
+++ TomcatLogging/src/main/java/us/fourfront/App/Service.java
@@ -0,0 +1,52 @@
+package us.fourfront.App;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext;
+
+
+@Path("/service")
+public class Service {
+
+	final static Logger logger = LogManager.getLogger(Service.class);
+	private String pn;
+	
+	public Service() {
+		
+		String threadName = Thread.currentThread().getName();
+
+		Pattern pattern = Pattern.compile("(\\d{4})");
+		Matcher portNumber = pattern.matcher(threadName);
+		
+		/*
+		 * Parse the thread name to get the port.
+		 * This port will be sued for routing during the logging process 
+		 */
+		if (portNumber.find()) {
+			ThreadContext.put("tcport", portNumber.group(1));
+			this.pn=portNumber.group(1);
+		} else {
+			ThreadContext.put("tcport", "App");
+		}
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/ping")
+	public String ping() {
+
+		String msg="*********** Service Ping Request *********** Port:" + this.pn;
+		logger.info(msg);
+		
+		return msg;
+
+	}
+}
